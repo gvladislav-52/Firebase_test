16d86c2e132d8b5eb5129e8fcec5fabbe892e0d8
gvladislav-52 <gvladislav_52@mail.ru>
2024-07-15 01:16:06 +0300

    first commit

---
+++ Firebase_Test/APIManager.swift
@@ -0,0 +1,12 @@
+//
+//  APIManager.swift
+//  Firebase_Test
+//
+//  Created by macbookbro on 15.07.2024.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseStorage
+import FirebaseDatabase
